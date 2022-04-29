9e74384ab9b8a98d4c21650fd69fa7034d56353c
giovanifranz <giovanifranz151@gmail.com>
2022-04-29 17:58:53 -0300

    refactor: :sparkles: Melhria no código

--- src/components/AgendeButton.tsx
+++ src/components/AgendeButton.tsx
@@ -1,38 +1,37 @@
-import { Box, BoxProps, Button } from '@chakra-ui/react'
+import { Box, Button, ButtonProps } from '@chakra-ui/react'
 
-interface Props extends BoxProps {
+interface Props extends ButtonProps {
   border: string
 }
 
 function AgendeButton({ border, ...rest }: Props) {
   return (
-    <Box {...rest}>
-      <Button
-        w="185px"
+    <Button
+      w="185px"
+      h="185px"
+      borderRadius="50%"
+      bgColor="beige.200"
+      fontFamily="Cormorant Garamond"
+      fontSize="3xl"
+      fontWeight="bold"
+      color="brown.700"
+      transform="rotate(-10deg)"
+      position="relative"
+      {...rest}
+    >
+      <Box
         h="185px"
+        w="185px"
+        position="absolute"
+        border={border}
         borderRadius="50%"
-        bgColor="beige.200"
-        fontFamily="Cormorant Garamond"
-        fontSize="3xl"
-        fontWeight="bold"
-        color="brown.700"
-        transform="rotate(-10deg)"
-        position="relative"
-      >
-        <Box
-          h="185px"
-          w="185px"
-          position="absolute"
-          border={border}
-          borderRadius="50%"
-          ml="15px"
-          mt="15px"
-          filter="drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
-          onClick={() => window.scrollTo({ top: 99999, behavior: 'smooth' })}
-        />
-        Agende
-      </Button>
-    </Box>
+        ml="15px"
+        mt="15px"
+        filter="drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
+        onClick={() => window.scrollTo({ top: 99999, behavior: 'smooth' })}
+      />
+      Agende
+    </Button>
   )
 }
 
