f64ba808dda3e826d9205b9bf2f98b07cf821bd1
giovanifranz <giovanifranz151@gmail.com>
2022-04-29 15:07:37 -0300

    refactor: :heavy_minus_sign: remoção do prismic do projeto

--- src/services/prismic.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from '@prismicio/client'
-
-export function createClient(req?: unknown) {
-  const prismic = new Client(process.env.PRISMIC_ENDPOINT as string)
-  return prismic
-}
