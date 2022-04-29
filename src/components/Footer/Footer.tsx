3ed2a534d369b7f54196c1f1ed3c8b852c905845
giovanifranz <giovanifranz151@gmail.com>
2022-04-29 16:42:28 -0300

    style: :lipstick: melhorias na responsividade do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -21,14 +21,14 @@ export function Footer() {
   }
 
   return (
-    <Box as="footer" w="100%" borderTop={`1px solid ${brown}`} position="relative">
+    <Box as="footer" w="100%" borderTop={`1px solid ${brown}`} position="relative" py="40px">
       <Flex w={['95%', '70%']} mx="auto" pt="10" flexDir={['column-reverse', 'row']}>
         {isWideVersion && <Photo />}
         <Form />
         <SocialMedia {...contato} />
       </Flex>
       {isWideVersion && (
-        <Box as="div" position="absolute" bottom={0} right={20} w="155px">
+        <Box as="div" position="absolute" bottom="40px" right="20px" w="155px">
           <NextLink href="https://www.highlandertech.com.br/" passHref>
             <ChakraLink as={'a'} target="_blank" _hover={{ textDecoration: 'none' }}>
               <Text textAlign="right" fontSize="18px" color="brown.700">
